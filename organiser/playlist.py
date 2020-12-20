f5fe46efdeb4e506e796f0fe8a80e12afe22ba9b
Giorgio Momigliano <gmomigliano@gmail.com>
2020-12-21 00:58:35 +0100

    Add creation of monthly playlist when nonexistent

--- organiser/playlist.py
+++ organiser/playlist.py
@@ -2,13 +2,14 @@ from typing import List
 
 import spotipy
 from datetime import datetime
+from organiser.user import get_user_id
 
 
 def _get_monthly_playlist_name():
     return datetime.utcnow().strftime("%B %Y")
 
 
-def _find_playlist(sp: spotipy.Spotify, playlist_name: str, offset: int = 0):
+def _find_playlist_id(sp: spotipy.Spotify, playlist_name: str, offset: int = 0):
     playlists_object = sp.current_user_playlists(limit=50, offset=offset)
     playlists = playlists_object['items']
 
@@ -18,15 +19,25 @@ def _find_playlist(sp: spotipy.Spotify, playlist_name: str, offset: int = 0):
 
     # Handle paging
     if playlists_object['next']:
-        return _find_playlist(sp, playlist_name, offset + 50)  # 50 is max playlists returned
+        return _find_playlist_id(sp, playlist_name, offset + 50)  # 50 is max playlists returned
 
     return None
 
 
-def get_monthly_playlist(sp: spotipy.Spotify):
-    return _find_playlist(sp, _get_monthly_playlist_name())
+def _create_playlist(sp: spotipy.Spotify, playlist_name: str):
+    return sp.user_playlist_create(get_user_id(sp), playlist_name)
+
+
+def _get_monthly_playlist_id(sp: spotipy.Spotify):
+    playlist_name = _get_monthly_playlist_name()
+
+    playlist_id = _find_playlist_id(sp, playlist_name)
+    if playlist_id:
+        return playlist_id
+
+    return _create_playlist(sp, playlist_name)['id']
 
 
 def add_songs_to_monthly_playlist(sp: spotipy.Spotify, uris: List[str]):
     if uris:
-        sp.playlist_add_items(get_monthly_playlist(sp), items=uris)
+        sp.playlist_add_items(_get_monthly_playlist_id(sp), items=uris)
---
+++ organiser/user.py
@@ -0,0 +1,5 @@
+import spotipy
+
+
+def get_user_id(sp: spotipy.Spotify):
+    return sp.current_user()['id']
