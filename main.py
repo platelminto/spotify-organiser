d4380a24960ff90efd0863b0d911c9a6dea6248d
Giorgio Momigliano <gmomigliano@gmail.com>
2020-12-22 14:53:40 +0100

    Print auth URL so can run headless

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from playlist import add_songs_to_monthly_playlist
 
 scope = "user-library-read playlist-read-private playlist-read-collaborative " \
         "playlist-modify-public playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(scope=scope, open_browser=False))
 
 add_songs_to_monthly_playlist(sp, get_newly_saved_songs(sp))
