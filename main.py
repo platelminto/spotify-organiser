850ec396eb7f222738f31656ee940df150b5a44d
Giorgio Momigliano <gmomigliano@gmail.com>
2020-12-22 23:49:48 +0100

    Remove spotipy cache config

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import spotipy
 
 from saved import get_newly_saved_songs
 from playlist import add_songs_to_monthly_playlist
-from config import CACHE_PATH
 
 scope = "user-library-read playlist-read-private playlist-read-collaborative " \
         "playlist-modify-public playlist-modify-private"
-sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(scope=scope, open_browser=False, cache_path=CACHE_PATH))
+sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(scope=scope, open_browser=False))
 
 add_songs_to_monthly_playlist(sp, get_newly_saved_songs(sp))
