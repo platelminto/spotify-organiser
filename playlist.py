c8d7b5fb73b92348d0dee5dab4ae92bfa2a9637a
Giorgio Momigliano <gmomigliano@protonmail.com>
2021-09-04 16:27:13 +0200

    Use end of description for playlist identification

--- playlist.py
+++ playlist.py
@@ -9,7 +9,11 @@ def _get_monthly_playlist_name():
     return datetime.utcnow().strftime("%B %Y")
 
 
-def _find_playlist_id(sp: spotipy.Spotify, playlist_name: str, offset: int = 0):
+def _get_monthly_playlist_identifier():
+    return datetime.utcnow().strftime("%m%y")
+
+
+def _find_playlist_id_by_name(sp: spotipy.Spotify, playlist_name: str, offset: int = 0):
     playlists_object = sp.current_user_playlists(limit=50, offset=offset)
     playlists = playlists_object['items']
 
@@ -19,23 +23,44 @@ def _find_playlist_id(sp: spotipy.Spotify, playlist_name: str, offset: int = 0):
 
     # Handle paging
     if playlists_object['next']:
-        return _find_playlist_id(sp, playlist_name, offset + 50)  # 50 is max playlists returned
+        return _find_playlist_id_by_name(sp, playlist_name, offset + 50)  # 50 is max playlists returned
 
     return None
 
 
-def _create_playlist(sp: spotipy.Spotify, playlist_name: str):
-    return sp.user_playlist_create(get_user_id(sp), playlist_name)
+def _find_playlist_id_by_description_identifier(sp: spotipy.Spotify, identifier: str, offset: int = 0):
+    playlists_object = sp.current_user_playlists(limit=50, offset=offset)
+    playlists = playlists_object['items']
+
+    for playlist in playlists:
+        if playlist.get('description', "").strip().endswith(identifier):
+            return playlist['id']
+
+    # Handle paging
+    if playlists_object['next']:
+        return _find_playlist_id_by_description_identifier(sp, identifier, offset + 50)  # 50 is max playlists returned
+
+    return None
+
+
+def _find_playlist_id(sp: spotipy.Spotify, identifier, default_name):
+    return (_find_playlist_id_by_description_identifier(sp, identifier) or
+            _find_playlist_id_by_name(sp, default_name))
+
+
+def _create_playlist(sp: spotipy.Spotify, playlist_name: str, description: str):
+    return sp.user_playlist_create(get_user_id(sp), playlist_name, description=description)
 
 
 def _get_monthly_playlist_id(sp: spotipy.Spotify):
     playlist_name = _get_monthly_playlist_name()
+    identifier = _get_monthly_playlist_identifier()
 
-    playlist_id = _find_playlist_id(sp, playlist_name)
+    playlist_id = _find_playlist_id(sp, identifier, playlist_name)
     if playlist_id:
         return playlist_id
 
-    return _create_playlist(sp, playlist_name)['id']
+    return _create_playlist(sp, playlist_name, description=identifier)['id']
 
 
 def add_songs_to_monthly_playlist(sp: spotipy.Spotify, uris: List[str]):
